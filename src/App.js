a34d29ef1957dec49381affd79aafb7e9baa5025
Frederik Moroder <frotti92@hotmail.com>
2019-04-14 13:42:48 +0200

    This Works
    But it looks like horse crap

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import RealTime from "./Pages/RealTime";
 import Products from "./Pages/Product";
 import SignIn from "./Pages/SignIn";
 import Navbar from "./Components/NavBar/Navbar";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      authUser: JSON.parse(localStorage.getItem('authUser'))
+      authUser: JSON.parse(localStorage.getItem("authUser")),
+      data: {}
     };
   }
 
@@ -22,26 +24,58 @@ class App extends Component {
     firebase.auth.onAuthStateChanged(user => {
       if (!user && this.state.auth) {
         this.setState({ auth: undefined });
-        localStorage.setItem('authUser', undefined)
+        localStorage.setItem("authUser", undefined);
         console.log("user signed out");
       } else {
-        this.setState({ authUser: user })
-        localStorage.setItem('authUser', JSON.stringify(user))
+        this.setState({ authUser: user });
+        localStorage.setItem("authUser", JSON.stringify(user));
       }
     });
+    //   axios.get('http://localhost:4000/users/Auth_UID').then(res => {
+    //   this.setState({data: res.data});
+    // });
   }
 
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" render={(props) => <Navbar {...props} authUser={this.state.authUser} />}/>
-          <Route exact path="/" render={() => <Dashboard authUser={this.state.authUser} />}/>
-          <Route exact path="/community" render={() => <Community authUser={this.state.authUser} />} />
-          <Route exact path="/myusage" render={() => <MyUsage authUser={this.state.authUser} />} />
-          <Route exact path="/realtime" render={() => <RealTime authUser={this.state.authUser} />} />
-          <Route exact path="/products" render={() => <Products authUser={this.state.authUser} />} />
-          <Route exact path="/signin" render={() => (<SignIn authUser={this.state.authUser}/>)}/>
+          <Route
+            path="/"
+            render={props => (
+              <Navbar {...props} authUser={this.state.authUser} />
+            )}
+          />
+          <Route
+            exact
+            path="/"
+            render={() => <Dashboard authUser={this.state.authUser} />}
+          />
+          <Route
+            exact
+            path="/community"
+            render={() => <Community authUser={this.state.authUser} />}
+          />
+          <Route
+            exact
+            path="/myusage"
+            render={() => <MyUsage authUser={this.state.authUser} />}
+          />
+          <Route
+            exact
+            path="/realtime"
+            render={() => <RealTime authUser={this.state.authUser} />}
+          />
+          <Route
+            exact
+            path="/products"
+            render={() => <Products authUser={this.state.authUser} />}
+          />
+          <Route
+            exact
+            path="/signin"
+            render={() => <SignIn authUser={this.state.authUser} />}
+          />
         </div>
       </BrowserRouter>
     );

6e7689f5d0e03273c1e8f18abb831172d4fd9ac4
Frederik Moroder <frotti92@hotmail.com>
2019-04-28 13:15:56 +0200

    SignUp corrected and removed + 2 names in community graph
    What wrote above is the things i changed

--- src/Utils/Firebase.js
+++ src/Utils/Firebase.js
@@ -2,6 +2,7 @@ import app from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import { firebaseConfig } from "../Config/firebaseConfig";
+import moment from "moment";
 
 class Firebase {
   constructor() {
@@ -16,6 +17,18 @@ class Firebase {
     this.auth.signInWithEmailAndPassword(email, password);
 
   doSignOut = () => this.auth.signOut();
+
+  getTimeStamp = () => {
+    const timeStamp = new app.firestore.Timestamp.fromDate(
+      new Date(
+        moment("2010-01-01 00:00:00", "YYYY-MM-DD HH:mm:SS").format(
+          "YYYY-MM-DDTHH:mm:SS"
+        )
+      )
+    );
+
+    return timeStamp;
+  };
 
   doSignUpWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
